7b9a3ec9f29ecfa561b62de3513e1f4b8b5a846d
ratiborU <103406361+ratiborU@users.noreply.github.com>
2022-11-11 00:18:30 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ import "./big-picture.js";
 
 let pictures = document.querySelector('.pictures');
 let posts = createPostList(25);
-console.log(posts[0]);
+
 
 pictures.append(createPosts(posts, templatePhoto));
